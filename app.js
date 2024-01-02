6c7bedaa8d06a9dbdf1332a0fca184b593466569
bernardobaez <bernardbaez.beno@gmail.com>
2024-01-01 22:09:30 -0400

    ...

--- app.js
+++ app.js
@@ -1,44 +1,57 @@
-const { Client, ButtonBuilder, ButtonStyle, ActionRowBuilder, ChannelType } = require('discord.js');
+const { Client, ButtonBuilder, ButtonStyle, ActionRowBuilder, ChannelType, EmbedBuilder } = require('discord.js');
 const client = new Client({ intents: [3276799] });
-
-let ticketCount = 0;
+const { token } = require('./config.json');
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("messageCreate", async message => {
-  if (message.content === '!tk') {
-    const button = new ButtonBuilder()
-      .setLabel("Add ticket")
-      .setStyle(ButtonStyle.Secondary)
-      .setCustomId("ticket-button");
+    if (message.content === '!tk') {
+        const embed = new EmbedBuilder()
+            .setTitle("Es un titulo")
+            .setDescription("Ejemplo")
+            .setColor("#3498db");
 
-    const btn = new ActionRowBuilder().addComponents(button);
+        const button = new ButtonBuilder()
+            .setLabel("Create Ticket")
+            .setStyle(ButtonStyle.Secondary)
+            .setCustomId("ticket-button");
 
-    message.reply({content: "¿Desea agregar un ticket?", components: [btn]});
-  }
+        const btn = new ActionRowBuilder().addComponents(button);
+
+        message.reply({ content: "¿Desea agregar un ticket?", components: [btn], embeds: [embed] });
+    }
 });
 
 client.on('interactionCreate', async interaction => {
-  if (!interaction.isButton()) return;
-  if (interaction.customId === 'ticket-button') {
-    ticketCount++;
-    interaction.reply(`Se ha agregado un ticket! ${ticketCount}`);
+    if (!interaction.isButton()) return;
+    if (interaction.customId === 'ticket-button') {
+        
+        const disabledButton = new ButtonBuilder()
+            .setLabel("Create Ticket")
+            .setStyle(ButtonStyle.Secondary)
+            .setCustomId("ticket-button")
+            .setDisabled(true);
 
-    let name = `historial-tickets-${new Date().getDate().toLocaleString()}`;
-    let historialChannel = interaction.guild.channels.cache.find(channel => channel.name === name);
+        const disabledBtnRow = new ActionRowBuilder().addComponents(disabledButton);
 
-    if (!historialChannel) {
-      const guild = interaction.guild;
-      historialChannel = await guild.channels.create({
-        name,
-        type: ChannelType.GuildText
-      });
+        interaction.update({
+            content: `Se ha agregado un ticket!`,
+            components: [disabledBtnRow],
+        });
+
+        const name = `grupo-privado`;
+        let historialChannel = await interaction.guild.channels.cache.find(channel => channel.name === name);
+
+        if (!historialChannel) {
+            const guild = interaction.guild;
+            historialChannel = await guild.channels.create({
+                name,
+                type: ChannelType.GuildText
+            });
+        }
     }
-    historialChannel.send(`Nuevo ticket creado por ${interaction.user.tag}. Ticket: ${ticketCount}`);
-  }
 });
 
-const TOKEN = '';
-client.login(TOKEN);
+client.login(token);
